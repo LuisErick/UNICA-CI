27d57f4983e67905908a275ea7c87de21b43fb09
Fabian Peña <f.pena.jacobo@gmail.com>
2014-06-16 18:47:24 -0500

    Cambios en first_register/new , colocar nuevo slide y ordenar

--- app/controllers/first_register_controller.rb
+++ app/controllers/first_register_controller.rb
@@ -3,10 +3,14 @@ class FirstRegisterController < ApplicationController
 		@languages = Language.all
 		@course_types = CourseType.all
 		@levels = Level.all
+		#Esto no debería ser all, debería cambiar con ajax
 		@packages = Package.all
 		@marital_statuses = MaritalStatus.all
+		@colleges = College.all
 		@sex = {'1' => 'Masculino', '0' => "Femenino"}
 		@student_types = {'1' => 'Si, ya he cursado', '0' => 'No, es la primera vez'}
+		@is_unica = {'1' => 'Si, soy un alumno de la UNICA', '0' => 'No, no pertenezco a la UNICA'}
+		@is_postgrade = {'0' => 'No tengo un posgrado', '1' => 'Magister', '2' => 'Doctorado'}
 	end
 	def create
 		render text: params[:birthday]
